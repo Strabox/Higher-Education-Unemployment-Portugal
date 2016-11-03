b0c23e97e66b7e2813794aa8871ee1b929203587
Artur Fonseca <artur.fonseca@live.com>
2016-11-03 16:34:16 +0000

    ...

--- src/script.js
+++ src/script.js
@@ -2,5 +2,5 @@
 function test() {
 	alert("a");
 }
-
+test();
 d3.select("body").append("svg").attr("width", 50).attr("height", 50).append("circle").attr("cx", 25).attr("cy", 25).attr("r", 25).style("fill", "purple");
