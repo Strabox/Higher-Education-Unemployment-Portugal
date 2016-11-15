17d937e3261f1b9e3775dee2861e17db1e092108
Strabox <pardal.pires@gmail.com>
2016-11-15 02:11:50 +0000

    ...

--- src/script.js
+++ src/script.js
@@ -1,32 +1,33 @@
+//########################################
+//#       Global Code to all Views       #
+//########################################
+
 var universityDataset;
 
+//########################################
+//#            University View           #
+//########################################
+
+function getUniversityAcronym(universityName){
+	var res = "";
+	var tokens = universityName.split(" ");
+	for(var i = 0; i < tokens.length; i++){
+		res += tokens[i][0];
+	}
+	return res;
+}
+
 d3.json("University.json",function (data) {
-	universityDataset = data;
-	
-	var temp = new Object();
-	var a = [];
-	for(var i = 0; i < universityDataset.length;i++){
-		if(temp[universityDataset[i].CodigoFaculdade] == null){
-			var universityNode = new Object();
-			universityNode.CodigoFaculdade = universityDataset[i].CodigoFaculdade;
-			universityNode.NomeFaculdade = universityDataset[i].NomeFaculdade;
-			universityNode.Anos = [];
-			temp[universityDataset[i].CodigoFaculdade] = universityNode;
-		}
-		temp[universityDataset[i].CodigoFaculdade].Anos.push({"Ano":universityDataset[i].Ano,
-							"PercentagemDesemprego":universityDataset[i].PercentagemDesemprego,
-							"TotalDesempregados":universityDataset[i].TotalDesempregados});
-	}
-
-	for (var key in temp) {
-		if (!temp.hasOwnProperty(key)) {
-			continue;
-		}
-		a.push(temp[key]);
-	}
-	
-	universityDataset = a.slice(0,4);
-
+	//Transforms the original file entries in hierarchical objects.
+	universityDataset = d3.nest()
+				.key(function(d) { d.Acronym = getUniversityAcronym(d.NomeFaculdade); return d.NomeFaculdade; })
+				.entries(data);
+						
+	universityDataset.sort(function (d1,d2) {
+							return d1.key < d2.key;
+						});
+						
+	universityDataset = universityDataset.slice(0,5);
 	generateUniversityVis();
 });
 
@@ -34,64 +35,65 @@ d3.json("University.json",function (data) {
 function generateUniversityVis(){
 	var width = 550;
 	var height = 275;
-	var padding = 40;
-	var spaceBetweenLines = 40;
-	var spaceBetweenCircles = 50;
+	var padding = 20;
+	var leftPadding = 40;
 	var maximumCircleRadius = 60;
 	
-	var indexMap = new Object();
-	var universityIndex = 0;
-	
 	var years = [2007,2008,2009];
-	var universities = [];
 	
 	var svg = d3.select("#universityVis")
 				.append("svg")
 				.attr("width",width)
 				.attr("height",height);
-				
+		
 	//Scale for the unemployment circles
 	var circleScale = d3.scaleLinear().domain([0,100]).range([0,maximumCircleRadius]);
-	var xscale = d3.scalePoint().domain(years).range([padding,width - padding]);
-	var yscale = d3.scalePoint().domain(universityDataset.map(function(d) { return d.NomeFaculdade; })).range([padding,height]);
+	var xscale = d3.scalePoint().domain(years).range([leftPadding*2,width - leftPadding]);
+	var yscale = d3.scalePoint().domain(universityDataset.map(function(d) { return d.key; })).range([padding*2,height - padding]);
 	
 	svg.selectAll("g")
-	.data(universityDataset)
-	.enter().append("g")
-			.attr("y",(function(d){return yscale(d.NomeFaculdade); }))
-			.attr("height",30)
+		.data(universityDataset)
+		.enter().append("g")
+			.attr("id",function(d) {return yscale(d.key); })
+			.attr("y",function(d) {return yscale(d.key); })
             .each(function (d,i,n) { 
-                d3.select(n[i]).selectAll("circle").data(d.Anos)
+                d3.select(n[i]).selectAll("circle").data(d.values)
                 .enter().append("circle")
                         .attr("r",function (d2) {return circleScale(d2.PercentagemDesemprego); })
-                        .attr("cy",yscale(d.NomeFaculdade))
-                        .attr("cx",function(d2) {return xscale(d2.Ano);});
+                        .attr("cy",yscale(d.key))
+                        .attr("cx",function(d2) {return xscale(d2.Ano);})
+						.append("title")
+							.text(function(d2) { 
+									var res = "Percentagem Desemprego: " + d2.PercentagemDesemprego + "%";
+									res += "\nTotal Desempregados: " + d2.TotalDesempregados;
+									res += "\nTotal Dimplomados: " + d2.TotalDiplomados;
+									res += "\nAno: " + d2.Ano;
+									return res;
+								});
                 
-            } );
+            });
 	
 	var xaxis = d3.axisTop().scale(xscale);
 	var yaxis = d3.axisLeft().scale(yscale);
-	svg.append("g").attr("transform","translate(0,20)").call(xaxis);
-	svg.append("g").attr("transform","translate(20,0)").call(yaxis);
-	
-	
-	
-	/*
-	svg.selectAll("circle")
-		.data(universityDataset)
-		.enter().append("circle")
-			.attr("r",function(d) { return circleScale(d.PercentagemDesemprego); })
-			.attr("cy",function(d,i) {  
-					var res = padding + universityIndex * spaceBetweenLines;
-					if(d.CodigoFaculdade in indexMap){
-						res = indexMap[d.CodigoFaculdade];
-					}
-					else{
-						indexMap[d.CodigoFaculdade] = res;
-						universityIndex++;							
-					}
-					return res; 
-				})
-			.attr("cx",function(d) { return xscale(d.Ano); });
-	*/		
+	svg.append("g").attr("transform","translate(0,"+padding+")").call(xaxis);
+	svg.append("g").attr("transform","translate("+leftPadding+",0)").call(yaxis);
+	svg.selectAll(".tick").append("title").text(function(d) { return d; });
 }
+
+//########################################
+//#       University Viewpoint           #
+//########################################
+
+//TODO
+
+//########################################
+//#       University Viewpoint           #
+//########################################
+
+//TODO
+
+//########################################
+//#       University Viewpoint           #
+//########################################
+
+//TODO
